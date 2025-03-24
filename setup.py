91c8cb81a5194bd7cd01ba9ed46b2159deae08d1
Abhirup Choudhury <me22b2043@iiitdm.ac.in>
2025-03-24 10:08:10 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'GenAI Project',
+    version= '0.0.0',
+    author= 'Abhirup Choudhury',
+    author_email= 'abhirupiiitdmk26@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
